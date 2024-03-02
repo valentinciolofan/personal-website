d0e3a673898b5f6ad089e3806db439bf5c0c3553
valentinciolofan <valentincatalin02@gmail.com>
2024-03-02 14:30:34 +0200

    landing page, about section and header responsive, also added some new styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,16 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			 backgroundImage: {
+        		'custom-gradient': 'linear-gradient(to right, #121212, #121212)',
+     		 },
+			 backgroundColor: {
+				'about-me': '#121212',
+				'light-dark': '#1E1E1F',
+			 }
+
+		},
 	},
 	plugins: [require("daisyui")],
 }
